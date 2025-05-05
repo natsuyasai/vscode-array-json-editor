f847706a20641af1da35cb10747db4d819ae0d33
natsuyasai <yasai038tomato@gmail.com>
2025-05-05 11:02:40 +0900

    画面へ内容表示する部分まで作成

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,19 +1,40 @@
 import { vscode } from "./utilities/vscode";
 import { VscodeButton } from "@vscode-elements/react-elements";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  function handleHowdyClick() {
+  const [jsonText, setJsonText] = useState("");
+  const [jsonObject, setJsonObject] = useState({});
+
+  window.addEventListener("message", (event) => {
+    const message = event.data; // The JSON data that the extension sent
+    console.log("Received message from extension:", message);
+
+    switch (message.type) {
+      case "init":
+      case "update":
+        console.log(message.text); // Handle the message from the extension
+        setJsonText(message.text);
+        setJsonObject(JSON.parse(message.text));
+        break;
+      default:
+        console.log("Unknown command: " + message.command);
+        break;
+    }
+  });
+
+  function handleSave() {
     vscode.postMessage({
-      command: "hello",
-      text: "Hey there partner! 🤠",
+      command: "save",
+      text: jsonText,
     });
   }
 
   return (
     <main>
-      <h1>Hello World!</h1>
-      <VscodeButton onClick={handleHowdyClick}>Howdy!</VscodeButton>
+      <p>{jsonText}</p>
+      <VscodeButton onClick={handleSave}>Save</VscodeButton>
     </main>
   );
 }
