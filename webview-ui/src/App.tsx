65536a5e231d7ad1e75696f919704a1e3f3c09f0
natsuyasai <yasai038tomato@gmail.com>
2025-05-05 11:17:53 +0900

    メッセージ用定義作成

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,11 +1,38 @@
 import { vscode } from "./utilities/vscode";
 import { VscodeButton } from "@vscode-elements/react-elements";
 import "./App.css";
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+import { UpdateMessage, Message, ReloadMessage } from "../../src/message/messageType";
 
 function App() {
   const [jsonText, setJsonText] = useState("");
   const [jsonObject, setJsonObject] = useState({});
+
+  const handleMessagesFromExtension = useCallback(
+    (event: MessageEvent<Message>) => {
+      if (event.data.type === "update") {
+        const message = event.data as UpdateMessage;
+        setJsonText(message.payload);
+      }
+    },
+    [jsonText]
+  );
+  useEffect(() => {
+    window.addEventListener("message", (event: MessageEvent<Message>) => {
+      handleMessagesFromExtension(event);
+    });
+
+    return () => {
+      window.removeEventListener("message", handleMessagesFromExtension);
+    };
+  }, [handleMessagesFromExtension]);
+
+  const handleReloadWebview = () => {
+    vscode.postMessage({
+      type: "reload",
+      payload: jsonText,
+    });
+  };
 
   window.addEventListener("message", (event) => {
     const message = event.data; // The JSON data that the extension sent
@@ -26,8 +53,8 @@ function App() {
 
   function handleSave() {
     vscode.postMessage({
-      command: "save",
-      text: jsonText,
+      type: "save",
+      payload: jsonText,
     });
   }
 
